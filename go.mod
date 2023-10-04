4a777e80338598bbf8f08eb8d8cb0b1f14d10277
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2023-10-04 14:28:58 +0100

    add server

--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module custom-launchpad
 go 1.21.0
 
 require (
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/rakyll/launchpad v0.1.0 // indirect
 	github.com/rakyll/portmidi v0.0.0-20191102002215-74e95e8bc9b1 // indirect
+	github.com/zmb3/spotify/v2 v2.4.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20210810183815-faf39c7919d5 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -6,6 +6,29 @@ import (
 )
 
 func main() {
+	ctx := context.Background()
+	config := &clientcredentials.Config{
+		ClientID:     os.Getenv("SPOTIFY_ID"),
+		ClientSecret: os.Getenv("SPOTIFY_SECRET"),
+		TokenURL:     spotifyauth.TokenURL,
+	}
+	token, err := config.Token(ctx)
+	if err != nil {
+		log.Fatalf("couldn't get token: %v", err)
+	}
+	httpClient := spotifyauth.New().Client(ctx, token)
+	httpClient := spotifyauth.New().Client(ctx, token)
+	client := spotify.New(httpClient)
+	msg, page, err := client.FeaturedPlaylists(ctx)
+	if err != nil {
+		log.Fatalf("couldn't get features playlists: %v", err)
+	}
+
+	fmt.Println(msg)
+	for _, playlist := range page.Playlists {
+		fmt.Println("  ", playlist.Name)
+	}
+
 	pad, err := launchpad.Open();
 	if err != nil {
 	    fmt.Printf("Error initializing launchpad: %v", err)
